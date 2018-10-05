9b7987dffaed2015cbc9f0b5e1eaf27f57dd9aec
usmansabir98 <usmansabir98@hotmail.com>
2018-10-05 18:39:41 +0500

    implement cache with redis

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,6 +35,13 @@ class HomeController extends Controller
 
         $posts = [];
 
+        $redisPosts = $redis->get('posts');
+        if($redisPosts){
+            $posts = json_decode($redisPosts);
+            return view('home')->with('posts', $posts);
+
+        }
+
         foreach ($popular as $value) {
             # code...
             $id = str_replace('post:', '', $value);
@@ -42,6 +49,9 @@ class HomeController extends Controller
             $post->views = $redis->get('post:'.$id.':views');
             array_push($posts, $post);
         }
+
+        $redis->set('posts', json_encode($posts, true));
+        $redis->expire('posts', 60);
 
         return view('home')->with('posts', $posts);
     }
